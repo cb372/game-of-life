48e10a92a7752ec4a5bf517bf7a69c7f6bc4f6bb
Chris Birchall <chris.birchall@gmail.com>
2018-01-01 19:41:10 +0000

    Implement game loop using Signal

--- src/Main.purs
+++ src/Main.purs
@@ -1,34 +1,29 @@
 module Main where
 
-import Prelude
+import Prelude (Unit, bind, ($))
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Data.Maybe (Maybe(..))
-import Graphics.Canvas
-import Game
-import CanvasView
+import Graphics.Canvas (CANVAS, CanvasElement, getCanvasElementById, getContext2D)
+import Game as G
+import CanvasView (paintBoard)
+import Signal (foldp, runSignal, (~>))
+import Signal.Time (every, second)
 
-{-
-TODO
-implement CanvasDisplay
-implement UI to initialise cells and start iterations
-ref to Game
-setInterval to iterate game state
--}
 main :: forall e. Eff (console :: CONSOLE, canvas :: CANVAS | e) Unit
 main = do
   canvas <- getCanvasElementById "game"
   case canvas of
-    Just c -> useCanvas c
+    Just c -> run c
     Nothing -> displayError
  
-board :: Board
-board = setCell (mkEmptyBoard (Width 10) (Height 10)) { x: 3, y: 4} true
+initialBoard :: G.Board
+initialBoard = G.setCell (G.mkEmptyBoard (G.Width 10) (G.Height 10)) { x: 3, y: 4} true
 
-useCanvas :: forall e. CanvasElement -> Eff (canvas :: CANVAS | e) Unit
-useCanvas canvas = do
+run :: forall e. CanvasElement -> Eff (canvas :: CANVAS, console :: CONSOLE | e) Unit
+run canvas = do
   ctx <- getContext2D canvas
-  paintBoard ctx board
+  runSignal $ (foldp (\_ -> G.iterate) initialBoard (every second)) ~> (paintBoard ctx)
 
 displayError :: forall e. Eff (console :: CONSOLE | e) Unit
 displayError = log "Looks like your browser doesn't support Canvas"
